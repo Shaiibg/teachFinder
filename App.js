2d342d1d934ab724231806ef21c0bbd85d9f9402
Shai <shaibanuelos922@gmail.com>
2024-09-22 17:59:25 -0600

    firts commit

---
+++ App.js
@@ -0,0 +1,92 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.login}>
+        <Text style={styles.titulo}>Iniciar Sesión</Text>
+        <TextInput style={styles.textInput} placeholder='Correo Electrónico' />
+        <TextInput style={styles.textInput} placeholder='Contraseña'/>
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.singUp}>
+        <Text style={styles.subtitle}>¿Aún no tienes una cuenta?</Text>
+        <Text style={styles.text}>Regístrate para que puedas iniciar sesión</Text>
+        <TouchableOpacity style={styles.singButton}>
+          <Text style={styles.singButtonText}>Regístrarse</Text>
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',//Permite que los contenedores estén alineados.
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    padding: 20,
+  },
+  login:{
+    flex: 1,
+    borderLeftWidth: 9,
+    borderLeftColor: '#FF1111',
+    padding: 10,
+  },
+  singUp:{
+    flex: 1,
+    backgroundColor: '#FF1111',
+    padding: 30,
+  },
+  titulo: {
+    fontSize: 32,
+    color: '#FF1111',
+    marginBottom: 20,
+    paddingVertical: 5,
+    textAlign: 'center',
+  },
+  subtitle:{
+    fontSize: 24,
+    color: '#fff',
+    paddingVertical: 5,
+  },
+  textInput: {
+    width: '100%',
+    padding: 10,
+    marginBottom: 15,
+    backgroundColor: '#f0f0f0',
+  },
+  text: {
+    color: '#fff',
+    paddingVertical: 20,
+  },
+  loginButton: {
+    backgroundColor: 'red',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+
+  },
+  singButton:{
+    borderWidth: 2,
+    borderColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 14,
+  },
+  singButtonText: {
+    color: 'white',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+
+});
